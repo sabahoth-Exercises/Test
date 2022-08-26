205964ccae070f4aa0a2120f0532dc4d2ee81f56
Brunotte Wasaulua <82088279+sabahoth01@users.noreply.github.com>
2022-08-26 13:49:35 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/authentification');
 const userCtrl = require('../controller/user');
 const rateLimiterToken = require("../middleware/rate_limit_token");
 
-router.post('/signup',auth, rateLimiterToken, userCtrl.signup);
-router.post('/login',auth, rateLimiterToken, userCtrl.login);
+router.post('/signup', rateLimiterToken, userCtrl.signup);
+router.post('/login', rateLimiterToken, userCtrl.login);
 
 module.exports = router;
